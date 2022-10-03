d10347d0f898c3e2b6ffe502280f600e327ff77a
Charles <charlesarchy09@gmail.com>
2022-10-03 17:52:49 -0500

    change eslintrc.js rule

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images:{
     domains:['res.cloudinary.com', 'image.tmdb.org'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
